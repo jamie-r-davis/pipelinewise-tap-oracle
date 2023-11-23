2fc7dfd1f67459995f0d0d27fe0d01c955a84958
Jamie Davis <17893777+jamie-r-davis@users.noreply.github.com>
2023-11-22 21:01:03 -0500

    Update db.py
    Add support for dsn to be passed directly in the config

--- tap_oracle/db.py
+++ tap_oracle/db.py
@@ -10,6 +10,10 @@ def make_dsn(config):
    return cx_Oracle.makedsn(config["host"], config["port"], config["sid"])
 
 def open_connection(config):
-    LOGGER.info("dsn: %s", make_dsn(config))
-    conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
+    if 'dsn' in config:
+        LOGGER.info("dsn: %s", config['dsn'])
+        conn = cx_Oracle.connect(user=config['user'], password=config['password'], dsn=config['dsn'])
+    else:
+        LOGGER.info("dsn: %s", make_dsn(config))
+        conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
     return conn
